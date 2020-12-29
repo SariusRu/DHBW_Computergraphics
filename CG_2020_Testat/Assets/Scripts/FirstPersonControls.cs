06acda1ed9745814fd7176001f85fe13b89a2239
Samuel Rundel <samuel.rundel@live.de>
2020-12-29 13:59:14 +0100

    Surrounding

--- CG_2020_Testat/Assets/Scripts/FirstPersonControls.cs
+++ CG_2020_Testat/Assets/Scripts/FirstPersonControls.cs
@@ -2,182 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+
 public class FirstPersonControls : MonoBehaviour {
 
-    // Basic behaviour variables
-    public float movementSpeed = 1F;
-    public float rotationSpeed = 10F;
+    public Camera FirstPersonCamera;
+    public Camera StartCamera;
 
-    // Max Rotation Values
-    public float minX = -360F, maxX = 360F, minY = -90, maxY = 90;
-    public float xRot = 0, yRot = 0;
-    public bool useGravity = false;
+    public float movementSpeed = 7.5f;
+    public float jumpSpeed = 7.5f;
 
-    private GameObject physicalParent;
-    private Rigidbody physicalParentBody;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 45.0f;
+
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+
+    [HideInInspector]
+    public bool canMove = true;
 
     // Use this for initialization
     void Start () {
-        // Set gravity vector
-        Physics.gravity = new Vector3(0, -9.81F, 0);
 
-        // Get the physical body of the camera which is represented by a capsule
-        physicalParent = this.transform.parent.gameObject;
+        characterController = GetComponent<CharacterController>();
 
-        // Give the capsule a rigidbody for the appliance of phyiscs
-        physicalParentBody = physicalParent.AddComponent<Rigidbody>();
-
-        // Standard mode without gravity
-        physicalParentBody.useGravity = false;
-
-        // Switch of wind
-        physicalParentBody.angularDrag = 0;
-
-        // Stop physics in x and z direction for the camera's body so  there isnt any unwanted movement except for gravity
-        physicalParentBody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-
-        // Lock the cursor in the middle of the screen to achieve a fps handling
-        if (Cursor.lockState != CursorLockMode.Locked)
-            Cursor.lockState = CursorLockMode.Locked;
+        // Lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        MouseController();
-        KeyboardController();
-	}
+        // We are grounded, so recalculate move direction based on axes
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        float curSpeedX = canMove ? ( movementSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? ( movementSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
-    void MouseController()
-    {
-
-        // Clamp is used tp apply limits to either side of the rotation's value
-        // Save the mouse movements of the single axis into variables
-        xRot += Mathf.Clamp(rotationSpeed * Input.GetAxis("Mouse X"), minX, maxX);
-        yRot -= Mathf.Clamp(rotationSpeed * Input.GetAxis("Mouse Y"), minY, maxY);
-
-        yRot = Mathf.Clamp(yRot, -90, 90);
-
-        // Get a euler quanternion object, which saves a transformation without the z-axis.
-        // This is used so the camera doesnt start to tilt and stays in some form parallel to the ground like in common fps games.
-        Quaternion targetRot = Quaternion.Euler(new Vector3(yRot, xRot, 0.0F));
-
-        // Apply the tranformation with the euler object. 
-        transform.rotation = physicalParent.transform.rotation = targetRot; 
-        
-        //Quaternion.Lerp(transform.rotation, targetRot, Time.deltaTime);  <-- alternative rotation. not so direct but smoother.
-
-    }
-
-    // Function handeling different Keyevents.
-    void KeyboardController()
-    {
-        // Free the mouse for debugging
-        if(Input.GetKey(KeyCode.Escape))
-            Cursor.lockState = CursorLockMode.None;
-
-        // Standard movement keys and the movement applied when pressing them
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-            physicalParent.transform.localPosition += transform.right * Time.deltaTime * movementSpeed;
-
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-            physicalParent.transform.localPosition -= transform.right * Time.deltaTime * movementSpeed;
-
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-            physicalParent.transform.localPosition += transform.forward * Time.deltaTime * movementSpeed;
-
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-            physicalParent.transform.localPosition -= transform.forward * Time.deltaTime * movementSpeed;
-
-        // Gravity toggle for a different experience
-        if (Input.GetKeyDown(KeyCode.G))
-            ToggleGravity();
-
-        // Sink or elevate vertically to the floor for a minecraft style movement
-        if (Input.GetKey(KeyCode.Space))
-            physicalParent.transform.localPosition += new Vector3(0, 1, 0) * Time.deltaTime * movementSpeed;
-
-        if (Input.GetKey(KeyCode.LeftShift))
-            physicalParent.transform.localPosition -= new Vector3(0, 1, 0) * Time.deltaTime * movementSpeed;
-    }
-
-    // Function handling the basing gravity toggle
-    void ToggleGravity()
-    {
-        if (useGravity)
+        if (Input.GetKey(KeyCode.Space) && canMove)
         {
-            // Set of gravity of the camera's phyiscal body and stops all movement imideatly 
-            physicalParentBody.useGravity = false;
-            physicalParentBody.velocity = Vector3.zero;
-            useGravity = false;
+            moveDirection.y = jumpSpeed;
         }
-        else if (!useGravity)
+        else if (Input.GetKey(KeyCode.LeftShift) && canMove)
         {
-            physicalParentBody.useGravity = true;
-            useGravity = true;
+            moveDirection.y = -jumpSpeed;
         }
-            
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }        
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+        moveDirection.y = movementDirectionY;
+
+        // Player and Camera rotation
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            FirstPersonCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-    void KeyboardController()
-    {
-        return 0;
-    }
-    */
-
-    /*
-yaw += speedH * Input.GetAxis("Mouse X");
-pitch -= speedV * Input.GetAxis("Mouse Y");
-
-transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
-print("Current");
-print(_currentPos);
-Vector3 rotation = new Vector3();
-rotation.y = Mathf.Clamp(Input.GetAxis("Mouse X"), minX, maxX);
-rotation.x = Mathf.Clamp(Input.GetAxis("Mouse Y"), minY, maxY);
-print(rotation);
-Quaternion targetPos = Quaternion.Euler(rotation);
-print("Target");
-print(targetPos);
-transform.rotation = Quaternion.LookRotation(rotation);
-_currentPos = targetPos;
-*/
 }
---
+++ CG_2020_Testat/Assets/Scripts/SwitchToAstronautFirstPerson.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchToAstronautFirstPerson : MonoBehaviour
+{
+    public Camera firstPersonCamera;
+    public Camera thirdPersonCamera;
+    public Camera OverviewCamera;
+
+    public GameObject TextToHide;
+
+    
+    private float counter = 0;
+    public float deadlineFirstSwitch = 12;
+    public float deadlineSecondSwitch = 30;
+
+    private bool FirstSwitchOccured = false;
+    private bool SecondSwitchOccured = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Stay 60 Seconds with the Main Camera (= Overview over the Galaxy
+        //switch to second camera
+        if(counter < deadlineFirstSwitch || counter < (deadlineFirstSwitch+deadlineSecondSwitch))
+        {
+            counter += Time.deltaTime;
+        }
+        if(counter >= deadlineFirstSwitch && !FirstSwitchOccured)
+        {
+            //Switch camera
+            OverviewCamera.enabled = false;
+            firstPersonCamera.enabled = false;
+            TextToHide.SetActive(true);
+            thirdPersonCamera.enabled = true;
+            //Prevents the camera from any further switches
+            counter += Time.deltaTime;
+            FirstSwitchOccured = true;
+        }
+        if(counter >= (deadlineFirstSwitch + deadlineSecondSwitch) && !SecondSwitchOccured)
+        {
+            OverviewCamera.enabled = false;
+            firstPersonCamera.enabled = true;
+            thirdPersonCamera.enabled = false;
+            //Prevents the camera from any further switches
+            counter += Time.deltaTime;
+            SecondSwitchOccured = true;
+        }
+        Debug.Log(counter);
+    }
+}
