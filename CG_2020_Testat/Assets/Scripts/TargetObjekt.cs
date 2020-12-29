1042c670c4e1b15365488c96d721dc87260a4b90
Samuel Rundel <samuel.rundel@live.de>
2020-12-29 01:24:46 +0100

    Rotate around: Allows to rotate an object around another with variable speeds for all three axis Target Objekt: The Directional lights can be in the center of the galaxy (=Sun). This script can be used for the lights to allows shine on their assigned planet/Object, causing the illusion of an actual light-emitting sun

---
+++ CG_2020_Testat/Assets/Scripts/TargetObjekt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetObjekt : MonoBehaviour
+{
+    Vector3 offset = new Vector3(0, 5, 0);
+    public GameObject target;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.transform.position, offset);
+    }
+}
---
+++ CG_2020_Testat/Assets/Scripts/rotateAround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateAround : MonoBehaviour
+{
+    public GameObject target;
+
+    public float speedX = 20;
+    public float speedY = 20;
+    public float speedZ = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(target.transform.position, Vector3.up, speedX * Time.deltaTime);
+        transform.RotateAround(target.transform.position, Vector3.right, speedY * Time.deltaTime);
+        transform.RotateAround(target.transform.position, Vector3.forward, speedZ * Time.deltaTime);
+    }
+}
